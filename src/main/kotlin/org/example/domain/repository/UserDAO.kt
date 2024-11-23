75d07edc0818dd984067392b083dbb683b086f6a
speakland <speakland2002@gmail.com>
2024-11-23 19:50:17 +0000

    Finished lab 5

--- src/main/kotlin/org/example/domain/repository/UserDAO.kt
+++ src/main/kotlin/org/example/domain/repository/UserDAO.kt
@@ -3,7 +3,13 @@ import org.jetbrains.exposed.sql.selectAll
 import org.jetbrains.exposed.sql.transactions.transaction
 import org.example.domain.User
 import org.example.domain.db.Users
+import org.example.domain.db.Users.email
+import org.example.domain.db.Users.name
 import org.example.utils.mapToUser
+import org.jetbrains.exposed.sql.SqlExpressionBuilder.eq
+import org.jetbrains.exposed.sql.deleteWhere
+import org.jetbrains.exposed.sql.insert
+import org.jetbrains.exposed.sql.update
 
 class UserDAO {
 
@@ -17,19 +23,43 @@ class UserDAO {
     }
 
     fun findById(id: Int): User?{
-        return null
+        return transaction {
+            Users.selectAll().where { Users.id eq id }
+                .map{mapToUser(it)}
+                .firstOrNull()
+        }
     }
 
     fun save(user: User){
+        transaction {
+            Users.insert {
+                it[name] = user.name
+                it[email] = user.email
+            }
+        }
     }
 
     fun findByEmail(email: String) :User?{
-        return null
+        return transaction {
+            Users.selectAll().where { Users.email eq email }
+                .map{mapToUser(it)}
+                .firstOrNull()
+        }
     }
 
-    fun delete(id: Int) {
+    fun delete(id: Int):Int {
+        return transaction{
+            Users.deleteWhere{ Users.id eq id }
+        }
     }
 
     fun update(id: Int, user: User){
+        transaction {
+            Users.update ({
+                Users.id eq id}) {
+                it[name] = user.name
+                it[email] = user.email
+            }
+        }
     }
 }
