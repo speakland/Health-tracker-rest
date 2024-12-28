77caba250783d1c2c68919598cf989eb1f2fc473
speakland <speakland2002@gmail.com>
2024-12-28 23:14:35 +0000

    Fixed issue with vue not being recognised

---
+++ src/main/resources/vue/views/user-profile.vue
@@ -0,0 +1,16 @@
+<template id="user-profile">
+  <div>
+    <form v-if="user">
+      <label class="col-form-label">User ID: </label>
+      <input class="form-control" v-model="user.id" name="id" type="number" readonly/><br>
+      <label class="col-form-label">Name: </label>
+      <input class="form-control" v-model="user.name" name="username" type="text"/><br>
+      <label class="col-form-label">Email: </label>
+      <input class="form-control" v-model="user.email" name="email" type="email"/><br>
+    </form>
+    <dt v-if="user">
+      <br>
+      <a :href="`/users/${user.id}/activities`">View User Activities</a>
+    </dt>
+  </div>
+</template>
